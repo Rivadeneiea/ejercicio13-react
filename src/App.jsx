cb14891ef39c56ac160dd2cfa4da0f98f643b4a1
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-03 11:14:54 -0300

    creo estado para guardar ciudad y pais y componente para mostrar por pantalla

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 function App() {
   const [ciudad, setCiudad] = useState("Tucuman");
   const [pais, setPais] = useState("ar");
   const [clima, setClima] = useState([]);
+  const [climaGeneral, setClimaGeneral] = useState({});
   useEffect(() => {
     consultarApi();
-  }, []);
+  }, [ciudad, pais]);
   const consultarApi = async () => {
     try {
       const respuesta = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Tucuman,ar&units=metric&appid=c11b26f5dbfad2055d463476688199cd`
+        `https://api.openweathermap.org/data/2.5/weather?${ciudad},${pais}&units=metric&appid=c11b26f5dbfad2055d463476688199cd`
       );
       const dato = await respuesta.json();
       setClima(dato.results);
       console.log(dato);
     } catch (error) {}
   };
+
   const selectorCiudad = (e) => {
     setCiudad(e.target.value);
   };
@@ -31,19 +34,22 @@ function App() {
       <h1>Clima del mundo</h1>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Pais</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Ingrese Pais"
+            onChange={selectorPais}
+            valiu={pais}
+          />
         </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+        <Form.Group
+          className="mb-3"
+          controlId="formBasicPassword"
+          onChange={selectorCiudad}
+          valiu={ciudad}
+        >
+          <Form.Label>ciudad</Form.Label>
+          <Form.Control type="text" placeholder="Ingrese ciudad" />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
---
+++ src/components/TarjetaClima.jsx
@@ -0,0 +1,5 @@
+const TarjetaClima = () => {
+  return <div></div>;
+};
+
+export default TarjetaClima;
